455af3f57671bd4489172118d19c12ef1479bfee
LA <LA>
2019-01-23 21:19:21 +0000

    fixed outputs again

--- Demo2Cam/outputs.tf
+++ Demo2Cam/outputs.tf
@@ -8,7 +8,9 @@ output "public_ip" {
   value = "${aws_instance.aws_instance_LA.public_ip}"
 }
 
-output " name" {
-  value = "${random_pet.citi-pet.key_name}"
+output "key_name" {
+  value = "${aws_instance.aws_instance_LA.key_name}"
 }
+
+
 
